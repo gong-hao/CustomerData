e43e28d00f4a189cf226075a3e59f48e0f78e414
Gordon <neverlostmine@gmail.com>
2015-07-09 12:08:50 +0800

    Add View Update EDMX

--- CustomerData/Models/DB.Context.cs
+++ CustomerData/Models/DB.Context.cs
@@ -28,5 +28,6 @@ namespace CustomerData.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+        public virtual DbSet<vw_簡易報表> vw_簡易報表 { get; set; }
     }
 }
